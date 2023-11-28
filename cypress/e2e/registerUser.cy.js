88a4718b7dc1dcda7744a1d75bd31dbabdc2c755
Adam Szurma <adam.szurma@gmail.com>
2023-11-28 13:34:44 +0100

    add faker and login test

--- cypress/e2e/registerUser.cy.js
+++ cypress/e2e/registerUser.cy.js
@@ -1,22 +1,17 @@
 /// <reference types="cypress" />
-describe('Registration Test', () => {
-    before(() => {
-        cy.visit('http://localhost:8080/parabank/register.htm')
-    })
-    it('Verifies user is able to register', () => {
-    cy.get('#customer\\.firstName').type('Adam')
-    cy.get('#customer\\.lastName').type('Szurma')
-    cy.get('#customer\\.address\\.street').type('123 Main')
-    cy.get('#customer\\.address\\.city').type('London')
-    cy.get('#customer\\.address\\.state').type('London')
-    cy.get('#customer\\.address\\.zipCode').type('NW1 123')
-    cy.get('#customer\\.phoneNumber').type('123 456 789')
-    cy.get('#customer\\.ssn').type('123456789')
-    cy.get('#customer\\.username').type('adam.szurma')
-    cy.get('#customer\\.password').type('password')
-    cy.get('#repeatedPassword').type('password')
-    cy.get('form').contains('Register').click()
 
-    cy.contains('Welcome')
+describe('User Registration and Login', () => {
+    beforeEach(() => {
+        cy.visit('http://localhost:8080/parabank/')
+    })
+
+    it('Verifies user is able to register', () => {
+        cy.visit('http://localhost:8080/parabank/register.htm')
+        cy.register()
+    })
+
+    it('Verifies user is able to login', () => {
+        cy.visit('http://localhost:8080/parabank/')
+        cy.login()
     })
 })
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,5 +1,5 @@
 /// <reference types="cypress" />
-
+import { faker } from "@faker-js/faker";
 // ***********************************************
 // This example commands.js shows you how to
 // create various custom commands and overwrite
@@ -12,6 +12,34 @@
 //
 //
 // -- This is a parent command --
+Cypress.Commands.add('register', () =>{
+let password = faker.internet.password()
+let username = faker.internet.userName()
+cy.get('#customer\\.firstName').type(faker.person.firstName())
+cy.get('#customer\\.lastName').type(faker.person.lastName())
+cy.get('#customer\\.address\\.street').type(faker.location.street())
+cy.get('#customer\\.address\\.city').type(faker.location.city())
+cy.get('#customer\\.address\\.state').type(faker.location.state())
+cy.get('#customer\\.address\\.zipCode').type(faker.location.zipCode())
+cy.get('#customer\\.phoneNumber').type(faker.phone.number())
+cy.get('#customer\\.ssn').type(faker.number.int({ min: 1000000, max: 9999999 }))
+cy.get('#customer\\.username').type(username)
+cy.get('#customer\\.password').type(password)
+cy.get('#repeatedPassword').type(password)
+cy.get('form').contains('Register').click()
+
+cy.contains(`Welcome ${username}`)
+})
+
+Cypress.Commands.add('login', () =>{
+    let password = faker.internet.password()
+    let username = faker.internet.userName()
+    cy.get('input[name="username"').type('john')
+    cy.get('input[name="password"').type('demo')
+    cy.get('form').contains('Log In').click()
+    
+    cy.contains('Welcome John')
+    })
 //
 //
 // -- This is a child command --
